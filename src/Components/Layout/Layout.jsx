f8973d517736f70f937578eec3bb68ab10e2518d
Nouh1408 <ahmedinouh@gmail.com>
2025-02-05 13:44:25 +0200

    W2S2 comp

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import styles from './Layout.module.css'
+import Navbar from '../Navbar/Navbar'
+import { Outlet } from 'react-router-dom'
 export default function Layout() {
   return (
-    <div className={styles}>Layout</div>
+    <>
+    <Navbar/>
+    <Outlet/>
+    </>
   )
 }
