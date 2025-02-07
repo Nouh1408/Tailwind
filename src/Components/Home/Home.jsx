e2342d0e2de06805aa8370a8bb605edbab1fa62f
Nouh1408 <ahmedinouh@gmail.com>
2025-02-07 11:07:34 +0200

    installed some packages

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './Home.module.css'
+
+
+
 export default function Home() {
+  const [DarkMode, setDarkMode] = useState(false)
+
+  useEffect(toggleMode,[DarkMode])
+
+  function toggleMode(){
+    const htmlElm = document.querySelector("html")
+    if(DarkMode){
+      
+    }
+  }
   return (
     <section className='p-10'>
-
+      
       <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
   <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
   <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
 </a>
+<button onClick={() => setDarkMode(!DarkMode)}>Toggler</button>
     <div className='bg-red-400 space-y-4 mt-10 p-3'>
         <h2 className='bg-sky-700 '>Lorem, ipsum.</h2>
         <h2 className='bg-sky-700 transition-all hover:duration-1000 delay-100 hover:bg-red-500 hover:transition'>Lorem, ipsum.</h2>
